5e23fdf933a0ef447c9962a634f19fa84e946258
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-13 15:11:16 -0500

    Create schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL 
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT 
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT
+);
